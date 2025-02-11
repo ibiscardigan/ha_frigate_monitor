7d791c2d5b5d934b3ab3a4d686e2d796046cb94e
Hamish <hamish_songsmith@icloud.com>
2025-02-11 21:48:09 +1100

    added dummy test

---
+++ tests/test_app.py
@@ -0,0 +1,8 @@
+"""
+Simple test to verify CI works at this stage
+"""
+
+
+def test_ci_validation():
+    """Simple test to validate CI workflow runs successfully."""
+    assert True  # Always passes
